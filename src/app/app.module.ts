a19c8729e6a95103ca82cb94af2a4c4909dd8353
Anil Dhulappanavar <anil.dhulappanavar@gmail.com>
2017-02-15 11:44:36 +0530

    Added Route with one route 404 Error component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router'
 
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 
+import { Error404Component } from './errors/404.component'
+import { appRoutes } from './routes'
+
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    Error404Component
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes) 
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router'
+import { Error404Component } from './errors/404.component'
+
+export const appRoutes : Routes = [
+  { path: '404', component: Error404Component }
+]
