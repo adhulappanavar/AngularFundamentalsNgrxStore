e0d50fd56924ddfc598c20bbdcc0a7e0ad225e68
Anil Dhulappanavar <anil.dhulappanavar@gmail.com>
2017-02-22 14:56:28 +0530

    JSON Server working, HTTP Service ready

--- src/app/services/events.service.ts
+++ src/app/services/events.service.ts
@@ -11,37 +11,37 @@ const BASE_URL = 'http://localhost:3000/events/';
 const HEADER = { headers: new Headers({ 'Content-Type': 'application/json' }) };
 
 @Injectable()
-export class ItemsService {
+export class EventsService {
   events: Observable<Array<IEvent>>;
 
   constructor(private http: Http, private store: Store<AppStore>) {
     this.events = store.select('events');
   }
 
-  loadItems() {
+  loadEvents() {
     this.http.get(BASE_URL)
       .map(res => res.json())
       .map(payload => ({ type: 'ADD_EVENTS', payload }))
       .subscribe(action => this.store.dispatch(action));
   }
 
-  saveItem(event: IEvent) {
-    (event.id) ? this.updateItem(event) : this.createItem(event);
+  saveEvent(event: IEvent) {
+    (event.id) ? this.updateEvent(event) : this.createEvent(event);
   }
 
-  createItem(event: IEvent) {
+  createEvent(event: IEvent) {
     this.http.post(`${BASE_URL}`, JSON.stringify(event), HEADER)
       .map(res => res.json())
       .map(payload => ({ type: 'CREATE_EVENT', payload }))
       .subscribe(action => this.store.dispatch(action));
   }
 
-  updateItem(event: IEvent) {
+  updateEvent(event: IEvent) {
     this.http.put(`${BASE_URL}${event.id}`, JSON.stringify(event), HEADER)
       .subscribe(action => this.store.dispatch({ type: 'UPDATE_EVENT', payload: event }));
   }
 
-  deleteItem(event: IEvent) {
+  deleteEvent(event: IEvent) {
     this.http.delete(`${BASE_URL}${event.id}`)
       .subscribe(action => this.store.dispatch({ type: 'DELETE_EVENT', payload: event }));
   }
